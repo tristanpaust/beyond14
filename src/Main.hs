b5e1a35279585a3546d4f953257819bd92089b79
Tristan Paust <tristanpaust@gmail.com>
2018-05-24 21:18:05 -0700

    basic mechanics with hardcoded values for now

--- src/Main.hs
+++ src/Main.hs
@@ -1,67 +1,184 @@
 module Main where
-
+  
+import Data.List (nubBy)
 import Data.Maybe
 import Control.Monad
 import Control.Applicative
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
+width, height, offset :: Int
+width = 400
+height = 600
+offset = 400
+
+window :: Display
+window = InWindow "Beyond 14" (width, height) (offset, offset)
+
+background :: Color
+background = dark green
+
+gameState :: [(Int, Maybe Int)]
+gameState = [
+  (1,  Just 1),
+  (2,  Just 1),
+  (3,  Just 1),
+  (4,  Nothing),
+  (5,  Just 2),
+  (6,  Just 3),
+  (7,  Nothing),
+  (8,  Nothing),
+  (9,  Nothing),
+  (10, Nothing),
+  (11, Nothing),
+  (12, Nothing),
+  (13, Nothing),
+  (14, Nothing),
+  (15, Nothing),
+  (16, Just 1)
+  ]
+-- *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ --
+
+data Tile = Tile {value :: (Int, Maybe Int)}
+  deriving Show
+
 type Coordinates = (Float, Float)
 
-data Move = Int
-
 data Board = Board
-  { numCoords :: [Coordinates] }
+  { numCoords :: [Coordinates], state :: [Tile], score :: Int }
 
-emptyBoard :: Board
-emptyBoard = Board []
+makeTile :: Int -> Maybe Int -> Tile
+makeTile a b = Tile {value = (a,b)}
 
-pushToken :: Coordinates -> Board -> Board
-pushToken c b =  b { numCoords = c : numCoords b}
+foo ((a,b):xs) = makeTile a b : foo xs
+foo [] = []
 
-type Size = Float
+makeBoard :: [(Int, Maybe Int)] -> Board
+makeBoard [] = Board {state = []}
+makeBoard ((a,b):xs) = Board {state = foo ((a,b):xs)}
 
-resize :: Size -> Path -> Path
-resize k = fmap (\ (x, y) -> (x * k, y * k))
+getNumber :: Maybe Int -> Int
+getNumber (Just number) = number
 
-drawNumber :: Size -> Coordinates -> Picture
-drawNumber k (x, y) =
-  let x' = k * (x - 0.2)
-      y' = k * (y - 0.25)
-  in color green $ translate x' y' $ scale 0.5 0.5 $ Text (show 2)
+drawTile :: Float -> Tile -> Picture
+drawTile x t@Tile{value=(a,b)} = 
+  let background = [color orange $ rectangleSolid 100 100,
+                    color green  $ rectangleSolid 95 95
+                   ]
+      number = if b /= Nothing 
+                 then [translate (-20) (-20) $ scale 0.5 0.5 $ text $ show $ (getNumber b)]
+                 else []
+    in pictures [ translate x 0 $ pictures $ background ++ number ]
 
-drawBoard :: Size -> Board -> Picture
-drawBoard k b = Pictures $ grid : ns where
-  ns = fmap (drawNumber k) $ numCoords b
+drawRow :: [Tile] -> Picture
+drawRow [a,b,c,d] = 
+  pictures [
+    drawTile 0 a,
+    drawTile 100 b,
+    drawTile 200 c,
+    drawTile 300 d
+    ]
 
-  grid :: Picture
-  grid = color black $ Pictures $ fmap (line . resize k)
-       [ [(-2, -1), (2 , -1)]
-       , [(-2, 0) , (2 , 0)]       
-       , [(-2, 1) , (2 , 1)]
-       , [(-1, -2), (-1, 2)]
-       , [(0 , -2), (0 , 2)]       
-       , [(1 , -2), (1 , 2)]
-       ]
+drawBoard :: Board -> Picture
+drawBoard b@Board{state=[a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16]} =
+  pictures [
+    translate (-150) 200 (drawRow [a1, a2, a3, a4]),
+    translate (-150) 100 (drawRow [a5, a6, a7, a8]),
+    translate (-150) 000 (drawRow [a9, a10, a11, a12]),
+    translate (-150) (-100) (drawRow [a13, a14, a15, a16])
+  ]
 
-checkCoordinate :: Size -> Float -> Maybe Float
-checkCoordinate k f' =
-  let f = f' / k
+test :: Tile
+test = makeTile 1 (Just 6)
+
+-- Initialize empty board that'll be used for the gui
+emptyBoard :: Picture
+emptyBoard = pictures [ drawBoard (makeBoard gameState) ]
+
+updateTile :: Int -> [(Int, Maybe Int)] -> [(Int, Maybe Int)]
+updateTile x ((a,b):xs)
+  | a == 4 = ((a,(Just x)):xs)
+  | otherwise = (a,b):updateTile x xs
+
+updateB :: Board -> Board
+updateB b = makeBoard (pushUpdates gameState 2)
+
+
+
+{--
+checkCoordinate :: Float -> Maybe Float
+checkCoordinate f' =
   in  (-1.5) <$ guard (-2 < f && f < -1)
-  <|> (-0.5)    <$ guard (-1 < f && f < 0)
+  <|> (-0.5) <$ guard (-1 < f && f < 0)
   <|> 0.5    <$ guard (0  < f && f < 1)
   <|> 1.5    <$ guard (1  < f && f < 2 )
+--} 
+handleKeys :: Event -> Board -> Board
+handleKeys (EventKey (MouseButton LeftButton) Down _ (x', y')) b =
+	updateB b
 
-handleKeys :: Size -> Event -> Board -> Board
-handleKeys k (EventKey (MouseButton LeftButton) Down _ (x', y')) b =
-  fromMaybe b $ do
-    x <- checkCoordinate k x'
-    y <- checkCoordinate k y'
-    return $ pushToken (x, y) b
-handleKeys k _ b = b
+handleKeys _ b = b   
 
 main :: IO ()
-main =
-  let window = InWindow "Tic Tac Toe" (400, 400) (10, 10)
-      size   = 100.0
-  in play window yellow 1 emptyBoard (drawBoard size) (handleKeys size) (flip const)
+main = play window background 1 (makeBoard gameState) drawBoard handleKeys (flip const)
+
+-- Get a tuple (value, index) of a certain element by its index
+getCurrentValue :: [(Int,Maybe Int)] -> Int -> (Int, Maybe Int)
+getCurrentValue [] index' = (0, Nothing)
+getCurrentValue ((index,tile):board) index' 
+  | index == index' = (index, tile)
+  | otherwise = getCurrentValue board index'
+
+-- Get one tile value and index. Same as above, just renamed for clarity
+getNeighbor :: [(Int,Maybe Int)] -> Int -> (Int, Maybe Int)
+getNeighbor [] index' = (0, Nothing)
+getNeighbor ((index,tile):board) index' 
+  | index == index' = (index,tile)
+  | otherwise = getNeighbor board index'
+
+-- Get all the neighbors that a tile has, as a list of tuples with indices and values
+getList :: [(Int,Maybe Int)] -> Int -> [(Int, Maybe Int)]
+getList board index = 
+  let neighbors = getNeighbor board in 
+    (
+       neighbors (index - 5):
+       neighbors (index - 4):
+       neighbors (index - 3):
+       neighbors (index - 1):
+       neighbors (index + 1):
+       neighbors (index + 3):
+       neighbors (index + 4):
+      [neighbors (index + 5)]
+    )
+
+-- Compare all values of neighboring tiles with the new tile and return a new sub-board in which all old values are turned into "Nothing" if they matched
+checkNeighborVals :: (Int, Maybe Int) -> [(Int, Maybe Int)] -> [(Int, Maybe Int)]
+checkNeighborVals (x,y) xs = [(v,Nothing) | (v,w) <- xs, w == y]
+
+-- Get the neighboring values already changed to "Nothing" from the function above and apply this sub-board to the actual game state board
+-- Then increase the new tile value by one if we had a value match
+-- Call yourself again recursively to check whether this update created a new value match situation
+pushUpdates :: [(Int, Maybe Int)] -> Int -> [(Int, Maybe Int)]
+pushUpdates board index = do
+    board' <- [checkNeighborVals (getCurrentValue board index) (getList board index)]
+    if (length board') > 0 then do
+        updatedBoard <- [(updateBoard board board')]
+        finalBoard <- [increaseCurrentValue updatedBoard (getCurrentValue board index)]
+        (pushUpdates finalBoard index)
+    else board
+
+-- Check for intersections between values in the actual gamestate and the new sub-board 
+-- Replace the old values with the new ones
+-- Return a full board
+updateBoard :: [(Int, Maybe Int)] -> [(Int, Maybe Int)] -> [(Int, Maybe Int)]
+updateBoard a [] = a
+updateBoard ((a,b):c) ((x,y):z)
+  | a == x = (x,y):(updateBoard c z)
+  | otherwise = (a,b):(updateBoard c ((x,y):z))
+
+-- Get the newly updated gameboard and the latest tile, find it in the board and increase its value by 1
+increaseCurrentValue :: [(Int, Maybe Int)] -> (Int, Maybe Int) -> [(Int, Maybe Int)]
+increaseCurrentValue [] (x, Just y) = [(x, Just y)]
+increaseCurrentValue ((a,b):c) (x, Just y)
+  | a == x = (x, Just (y + 1)):c
+  | otherwise = (a,b):increaseCurrentValue c (x, Just y)  
